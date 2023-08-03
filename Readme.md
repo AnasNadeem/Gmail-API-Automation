c781103f7a2516baa48689b30aad80a25edd6421
Anas Nadeem <anas5678go@gmail.com>
2023-08-03 15:47:22 +0530

    Gmail Authenticator file added

---
+++ Readme.md
@@ -0,0 +1,17 @@
+Before getting started with the projects you need to go through the following steps:
+## 1. Enable the Gmail API
+In the Google Cloud console, enable the Gmail API. [Enable the API](https://console.cloud.google.com/flows/enableapi?apiid=gmail.googleapis.com)
+
+## 2. Configure the OAuth consent screen
+If you're using a new Google Cloud project to complete this quickstart, configure the OAuth consent screen and add yourself as a test user. If you've already completed this step for your Cloud project, skip to the next section. [Follow these steps](https://developers.google.com/gmail/api/quickstart/python#configure_the_oauth_consent_screen)
+
+## 3. Authorize credentials for a desktop application
+
+[Follow these setps](https://developers.google.com/gmail/api/quickstart/python#authorize_credentials_for_a_desktop_application) to generate credentails.
+
+## 4. Setup environment
+```py -m venv env```
+
+## 5. Install required packages
+```pip install -r requirements.txt```
+
---
+++ authenticate.py
@@ -0,0 +1,39 @@
+# Below is modified snippet to authenticate to the Gmail API using the Google Python Client Library provided by Google. Check it out here https://developers.google.com/gmail/api/quickstart/python. I've modified it according to my usecase.
+
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+def authenticate(client_secret_file, scopes):
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', scopes)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                client_secret_file, scopes)
+            creds = flow.run_local_server(port=0)
+
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('gmail', 'v1', credentials=creds)
+        return service
+    except HttpError as error:
+        print(f'Error occurred in authenticate: {error}')
+        return None
